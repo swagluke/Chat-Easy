96e5b43e77f5deeef08b4443a3e804f7fbd916ea
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-26 19:38:31 -0400

    Done setting import.

--- Chat-Easy/src/environments/environment.ts
+++ Chat-Easy/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyDrmeSW52QNa5kaHF-w6YPhZcXtBWsdnbk",
+    authDomain: "zhangl-chat-easy.firebaseapp.com",
+    databaseURL: "https://zhangl-chat-easy.firebaseio.com",
+    projectId: "zhangl-chat-easy",
+    storageBucket: "zhangl-chat-easy.appspot.com",
+    messagingSenderId: "448396659958"
+    }
 };
