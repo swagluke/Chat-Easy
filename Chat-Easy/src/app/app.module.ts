25e8d6b6a4f3b54e0e42a0e9880e87d74a97189b
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-26 21:24:36 -0400

    Added ng g component signin and ng g component main.

--- Chat-Easy/src/app/app.module.ts
+++ Chat-Easy/src/app/app.module.ts
@@ -9,9 +9,13 @@ import { AngularFireModule } from 'angularfire2';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
+import { MainComponent } from './+main/main.component';
+import { SigninComponent } from './+signin/signin.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
