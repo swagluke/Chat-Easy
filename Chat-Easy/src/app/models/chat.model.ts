71b34809627bf81ff89667eda6bcce99bc39417c
Bart Miskowiec <miskowbs@rose-hulman.edu>
2017-04-27 06:58:28 -0400

    Completed chat model, added AuthorService

--- Chat-Easy/src/app/models/chat.model.ts
+++ Chat-Easy/src/app/models/chat.model.ts
@@ -1,12 +1,25 @@
+import { Post } from './post.model';
+import { Author } from './author.model';
 import { FirebaseFlatSnapshot } from './firebase-flat-snapshot.model';
 
 export class Chat extends FirebaseFlatSnapshot {
     public authorKeys: string[];
-    public posts: string[];
+    public postBodys: string[];
 
     constructor(obj?: any) {
         super(obj);
-        this.posts = obj && obj.posts || "";
+        this.postBodys = obj && obj.posts || "";
         this.authorKeys = obj && obj.authorKeys || "";
+    }
+}
+
+export class ChatWithPostsAndAuthors extends Chat {
+    public authors: Author[];
+    public posts: Post[];
+
+    constructor(obj?: any) {
+        super(obj);
+        this.authors = obj && obj.authors || {};
+        this.posts= obj && obj.posts || {};
     }
 }
--- Chat-Easy/src/app/services/author.service.ts
+++ Chat-Easy/src/app/services/author.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { FirebaseObjectObservable, AngularFire } from "angularfire2";
+import { Author } from "app/models/author.model";
 
 @Injectable()
 export class AuthorService {
+  readonly authorPath = 'authors';
 
-  constructor() { }
+  public authorMapStream: FirebaseObjectObservable<Map<string, Author>>;
+
+  constructor(private af: AngularFire) {
+    this.authorMapStream = this.af.database.object(`/${this.authorPath}`);
+  }
+
+  updateAuthor(authorKey: string, displayName: string, photoUrl: string): void {
+    const author = new Author({
+      displayName: displayName,
+      photoUrl: photoUrl
+    });
+    this.af.database.object(`/${this.authorPath}/${authorKey}`).set(author);
+  }
 
 }
